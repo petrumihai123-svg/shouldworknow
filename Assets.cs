a6a6da66dba7d9493f90036f7382914c12ca43e2
petrumihai123-svg <petrumihai123@gmail.com>
2026-01-17 18:34:18 +0000

    Update Assets.cs

--- Assets.cs
+++ Assets.cs
@@ -1,11 +1,23 @@
 namespace PortableWinFormsRecorder;
 
-// Reserved for future image-capture assets.
 public static class Assets
 {
     public static string GetAssetsDirNearScript(string scriptPath)
     {
         var dir = Path.GetDirectoryName(Path.GetFullPath(scriptPath)) ?? Environment.CurrentDirectory;
         return Path.Combine(dir, "script_assets");
+    }
+
+    public static string EnsureAssetsDir(string scriptPath)
+    {
+        var assets = GetAssetsDirNearScript(scriptPath);
+        Directory.CreateDirectory(assets);
+        return assets;
+    }
+
+    public static string NewPngName(string assetsDir, string prefix)
+    {
+        var stamp = DateTime.UtcNow.ToString("yyyyMMdd_HHmmss_fff");
+        return Path.Combine(assetsDir, $"{prefix}_{stamp}.png");
     }
 }
